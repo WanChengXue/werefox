d7d4c2d4ffac4666e4d0e3dc632fbad3028b6aa2
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2024-01-26 17:56:34 +0800

    1-26 17:56

--- werefox_backend/lib/werefox_backend/room_controller.ex
+++ werefox_backend/lib/werefox_backend/room_controller.ex
@@ -3,6 +3,11 @@ defmodule WerefoxBackend.RoomController do
     
     def init(room_id, config) do
         # 需要拆分config内容，然后交由agent_supervisor启动所有的智能体
+        room_host = WerefoxBackend.Agent.start_link(room_id, "host")
+
+        split_config(room_host, config)
+
+        WerefoxBackend.AgentSupervisor.start_link()
         {:ok, %{}}
     end
 
@@ -14,5 +19,12 @@ defmodule WerefoxBackend.RoomController do
 
     def start_link(room_id, config) do 
         GenServer.start_link(__MODULE__, {room_id, config})
+    end
+
+
+    defp split_config(room_host, config) do
+        %{
+            "agent_id_list" => ["1", "2", "3", "4", "5", "6"]
+        }
     end
 end
